85c5c37cae3b22b0272c0c592d6041eb4f22d664
Christian Henrique <30323772+ReiDaTecnologia@users.noreply.github.com>
2024-11-23 02:44:14 -0300

    Update RatUtils.java
    1: Remove what make the game crash if built.

--- src/main/java/com/github/alexthe666/rats/server/entity/RatUtils.java
+++ src/main/java/com/github/alexthe666/rats/server/entity/RatUtils.java
@@ -23,28 +23,16 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.pathfinding.PathNavigate;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.*;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.oredict.OreDictionary;
 
 import javax.annotation.Nullable;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class RatUtils {
-
-    public static final Set<Class<? extends Entity>> PLAGUE_IMMUNE_ENTITIES_CLASSES;
-    static {
-        PLAGUE_IMMUNE_ENTITIES_CLASSES = Arrays.stream(RatsMod.CONFIG_OPTIONS.plagueImmuneEntities).map(registryName -> {
-            EntityEntry entry = ForgeRegistries.ENTITIES.getValue(new ResourceLocation(registryName));
-            return entry == null ? null : entry.getEntityClass();
-        }).collect(Collectors.toSet());
-    }
 
     public static boolean isRatFood(ItemStack stack) {
         return (stack.getItem() instanceof ItemFood || isSeeds(stack) || stack.getItem() == Items.WHEAT) && stack.getItem() != RatsItemRegistry.RAW_RAT && stack.getItem() != RatsItemRegistry.COOKED_RAT;
